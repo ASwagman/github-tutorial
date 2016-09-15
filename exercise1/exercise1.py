07155c312589c442878f344cc13d28b6534ba0dd
ASwagman <Aprilswag@gmail.com>
2016-09-15 13:06:03 -0700

    added a third function to exercise1

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -10,6 +10,11 @@ def print_art(asciiArt):
 	print asciiArt
 
 
+#function for fellows
+def hi_fellow(status):
+	if status == "I am an Insight Fellow!":
+		print "That's awesome! How is that random forest model going?"
+
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
