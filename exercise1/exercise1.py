2bd760ebab3cc5e1060abfebff237492419a921a
ASwagman <Aprilswag@gmail.com>
2016-09-15 12:54:45 -0700

    created exercise1 and added one function

---
+++ exercise1/exercise1.py
@@ -0,0 +1,13 @@
+# an exercise from the Insight github tutorial 
+
+#function takes in a message and prints it
+def print_message(msg):
+	print msg 
+
+
+
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main(
+
